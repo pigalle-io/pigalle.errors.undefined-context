abf0c4efbc9d9363a325321c52ecc6f251286ff1
François Michaud <fmichaud@9fevrier.com>
2018-02-28 14:14:38 +0100

    Initial import

--- README.md
+++
@@ -1,2 +0,0 @@
-# pigalle.errors.undefinedcontext
-An exception to throw when the Pigalle context is missing. 
---
+++ test/undefined-context-error.test.js
@@ -0,0 +1,40 @@
+/**
+ * This file is part of pigalle.errors.undefined-context
+ *
+ * Copyright (c) 2018 SAS 9 Février.
+ *
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ *
+ */
+
+/* eslint-env mocha */
+
+require('should')
+
+const {UndefinedContextError} = require('../lib/undefined-context-error')
+
+describe('Class {UndefinedContextError}', () => {
+  it('should be a function', () => {
+    (UndefinedContextError).should.be.a.Function()
+  })
+})
+
+describe('Create a instance of {UndefinedContextError} using <new> keyword', () => {
+  it('should be an object', () => {
+    (new UndefinedContextError()).should.be.an.Object()
+  })
+
+  it('should be an instance of {UndefinedContextError}', () => {
+    (new UndefinedContextError()).should.be.an.instanceOf(UndefinedContextError)
+  })
+})
+
+describe('Throw a new {UndefinedContextError}', () => {
+  it('should throw an {Error}', () => {
+    (() => {throw new UndefinedContextError() }).should.throw(Error)
+  })
+
+  it('should throw an {UndefinedContextError}', () => {
+    (() => {throw new UndefinedContextError() }).should.throw(UndefinedContextError)
+  })
+})
